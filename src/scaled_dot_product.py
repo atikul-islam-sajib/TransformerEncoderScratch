7d08ec72832f281b462c1698760919b918e7e5a9
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2024-07-26 16:22:00 +0200

    scaled dot product prototype is added

---
+++ src/scaled_dot_product.py
@@ -0,0 +1,12 @@
+import sys
+import math
+import torch
+import torch.nn as nn
+
+sys.path.append("./src")
+
+
+def scaled_dot_product(
+    query: torch.Tensor, key: torch.Tensor, values: torch.Tensor, mask=None
+):
+    pass
